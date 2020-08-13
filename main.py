0d4959698cd444fb0f3b5bfa5e61fabe81a8e4d1
SantanaC4 <eduardo.ers@outlook.com>
2020-08-13 15:23:25 -0300

    features ouput layer

--- main.py
+++ main.py
@@ -3,8 +3,10 @@ from network import feedforward as ff
 from matplotlib import pyplot as plt
 from features import target_x
 from os import listdir
+from scipy  import spatial
 import pickle
 import sys
+import seaborn as sns
 import numpy as np
 
 #Carregando dataset  
@@ -34,7 +36,7 @@ images_testing, labels_testing = mndata.load_testing()
 #
 ########################features extraction##############################
 
-#Weights list
+#Save Weights list
 list_w = listdir("../weights_backup")
 
 
@@ -48,22 +50,58 @@ for w_path in list_w:
 
     targets_x = []
 
-    #Target neurons of out layer
+    #Target neurons of output layer
     neurons = 10
     for i in range(neurons):
         y = target_x.one_hot(10,0.01)[i]
         targets_x.append(target_x.input(x,y,w))
 
-    fig, ax = plt.subplots(nrows=2, ncols=5, constrained_layout=True)
+    #Print input neurons expected 
+    #fig, ax = plt.subplots(nrows=2, ncols=5, constrained_layout=True)
+    #flag = 0
+    #for i in range(2):
+    #    for j in range(5):
+    #        ax[i, j].imshow(targets_x[flag].reshape(28,28))
+    #        ax[i, j].set_title('Neuron {}'.format(flag),fontsize=10)
+    #        ax[i, j].axis('off')
+    #        flag += 1
 
-    flag = 0
-    for i in range(2):
-        for j in range(5):
-            ax[i, j].imshow(targets_x[flag].reshape(28,28))
-            ax[i, j].set_title('Neuron {}'.format(flag),fontsize=10)
-            ax[i, j].axis('off')
-            flag += 1
+    #fig.suptitle('{}'.format(w_path), y=0.98 ,fontsize=16)
+    #fig.savefig("../images/{}.png".format(w_path))
+    #plt.close()
 
-    fig.suptitle('{}'.format(w_path), y=0.98 ,fontsize=16)
-    fig.savefig("../images/{}.png".format(w_path))
-    plt.close()
+    #Cosine similarity
+    cosine_similarity = []
+    for i in range(10):
+            cosine_similarity.append([1 - spatial.distance.cosine(targets_x[i],targets_x[j]) for j in range(10)])
+
+    #Print right triangles of square matrix
+    mask = np.zeros_like(cosine_similarity)
+    mask[np.triu_indices_from(mask)] = True
+    with sns.axes_style("white"):
+        ax = sns.heatmap(cosine_similarity, mask=mask, vmax=.3, square=True,  cmap="YlGnBu")
+        plt.title('{}'.format(w_path), y=0.98 ,fontsize=16)
+        plt.savefig("../cosine_similarity/{}.png".format(w_path))
+        plt.close()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
